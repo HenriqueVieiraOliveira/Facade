9cc528b4428b6b9e50e5f401937aa146b7b96a8d
HenriqueVieiraOliveira <henrique.vieira.tech@gmail.com>
2025-04-22 21:08:26 -0300

    Adicionando estrutura

---
+++ Facade/src/br/fastfood/facade/ComboFacade.java
@@ -0,0 +1,11 @@
+package br.fastfood.facade;
+
+import br.fastfood.model.Combo;
+
+public class ComboFacade {
+    public Combo CriarCombo(int codigo){
+        Combo combo = new Combo();
+
+        return combo;
+    }
+}
